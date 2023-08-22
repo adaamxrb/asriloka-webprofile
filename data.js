c164ae0d899c56c828636e6c728052fc2bcfbff6
erikaprls <erika223p@gmail.com>
2023-08-22 23:22:58 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const servicesData = [
+    {
+        title: "Berpengalaman",
+        category: "berpengalaman",
+        description: ['(tes pengalaman) Asriloka', 'adalah pilihan', 'ideal untuk tujuan wisata alam, Asriloka sangat direkomendasikan, karena di sini Anda dapat merasakan kombinasi sempurna antara pengalaman berwisata menyenangkan, layanan profesional yang tak tertandingi, konsep cerdas, serta memiliki reputasi yang kaya akan pengalaman dan profesionalisme.Asriloka tidak hanya menawarkan permainan yang menyenangkan. Dengan layanan yang cerdas dan menyenangkan, Asriloka memberikan pengalaman tak terlupakan bagi para wisatawan. Jadi, jika Anda mencari pengalaman alam yang cerdas, profesional, dan penuh kesenangan, Asriloka adalah pilihan yang tepat.']
+    },
+    {
+        title: "Profesional",
+        category: "profesional",
+        description: [' PROF Asriloka adalah pilihan ideal untuk tujuan wisata alam, Asriloka sangat direkomendasikan, karena di sini Anda dapat merasakan kombinasi sempurna antara pengalaman berwisata menyenangkan, layanan profesional yang tak tertandingi, konsep cerdas, serta memiliki reputasi yang kaya akan pengalaman dan profesionalisme.Asriloka tidak hanya menawarkan permainan yang menyenangkan. Dengan layanan yang cerdas dan menyenangkan, Asriloka memberikan pengalaman tak terlupakan bagi para wisatawan. Jadi, jika Anda mencari pengalaman alam yang cerdas, profesional, dan penuh kesenangan, Asriloka adalah pilihan yang tepat.']
+    },
+    {
+        title: "Smart & Fun",
+        category: "smart&fun",
+        description: ['SMART Asriloka adalah pilihan ideal untuk tujuan wisata alam, Asriloka sangat direkomendasikan, karena di sini Anda dapat merasakan kombinasi sempurna antara pengalaman berwisata menyenangkan, layanan profesional yang tak tertandingi, konsep cerdas, serta memiliki reputasi yang kaya akan pengalaman dan profesionalisme.Asriloka tidak hanya menawarkan permainan yang menyenangkan. Dengan layanan yang cerdas dan menyenangkan, Asriloka memberikan pengalaman tak terlupakan bagi para wisatawan. Jadi, jika Anda mencari pengalaman alam yang cerdas, profesional, dan penuh kesenangan, Asriloka adalah pilihan yang tepat.']
+    },
+    {
+        title: "Provider Terbaik",
+        category: "providerterbaik",
+        description: ['PROVIDER Asriloka adalah pilihan ideal untuk tujuan wisata alam, Asriloka sangat direkomendasikan, karena di sini Anda dapat merasakan kombinasi sempurna antara pengalaman berwisata menyenangkan, layanan profesional yang tak tertandingi, konsep cerdas, serta memiliki reputasi yang kaya akan pengalaman dan profesionalisme.Asriloka tidak hanya menawarkan permainan yang menyenangkan. Dengan layanan yang cerdas dan menyenangkan, Asriloka memberikan pengalaman tak terlupakan bagi para wisatawan. Jadi, jika Anda mencari pengalaman alam yang cerdas, profesional, dan penuh kesenangan, Asriloka adalah pilihan yang tepat.']
+    }
+]
